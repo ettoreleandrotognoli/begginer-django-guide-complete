9e5807809183586f4af8060bf45476cc87d181aa
cesartalves <cesartorralvoa@hotmail.com>
2017-10-19 12:19:04 -0200

    parte 3 finalizada

--- boards/tests.py
+++ boards/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from .views import home, board_topics, new_topic
 from django.contrib.auth.models import User
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 # Create your tests here.
 
@@ -103,15 +104,7 @@ class NewTopicTests(TestCase):
         self.assertTrue(Topic.objects.exists())
         self.assertTrue(Post.objects.exists())
 
-    def test_new_topic_invalid_post_data(self):
-        '''
-        Invalid post data should not redirect
-        The expected behavior is to show the form again with validation errors
-        '''
-        url = reverse('new_topic', kwargs={'pk': 1})
-        response = self.client.post(url, {})
-        self.assertEquals(response.status_code, 200)
-
+   
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
         Invalid post data should not redirect
@@ -126,3 +119,27 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+
+
+    def test_contains_form(self):
+    	url = reverse('new_topic', kwargs={'pk':1})
+    	response = self.client.get(url)
+    	form = response.context.get('form')
+    	self.assertIsInstance(form, NewTopicForm)
+
+# assertIsInstance method . 
+# Basically we are grabbing the form instance in 
+# the context data, and checking if it is a NewTopicForm    	
+
+    def test_new_topic_invalid_post_data(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
+
+
